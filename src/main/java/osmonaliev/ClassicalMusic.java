7230d042e13634e6f1a70769f59efc63e634c937
Рыскелди <ryskeldi.osmonaliev@mail.ru>
2022-12-04 20:26:10 +0300

    commit

--- src/main/java/osmonaliev/ClassicalMusic.java
+++ src/main/java/osmonaliev/ClassicalMusic.java
@@ -1,12 +1,19 @@
 package osmonaliev;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Neil Alishev
  */
 @Component
+//@Scope("prototype")
 public class ClassicalMusic implements Music {
+//    @PostConstruct
+//    public void doMyInit(){
+//        System.out.println("doing my initializstion");
+//    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/osmonaliev/MusicPlayer.java
+++ src/main/java/osmonaliev/MusicPlayer.java
@@ -2,6 +2,7 @@ package osmonaliev;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,6 +20,27 @@ public class MusicPlayer {
 
     private Music music1;
     private Music music2;
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 @Autowired
     public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
         this.music1 = music1;
